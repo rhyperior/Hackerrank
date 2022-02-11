52f7830044447ddd08dfdf4436ed9a8ff3afb143
Shashi Shekhar <fantabulousshashi@gmail.com>
2022-02-11 16:23:32 +0530

    socks_pair_match problem

---
+++ socks_pair_match/socks_pair_match.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    pass
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = map(int, input().rstrip().split())
+    
+    print(ar)
+    # result = sockMerchant(n, ar)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
